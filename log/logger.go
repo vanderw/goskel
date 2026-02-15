6fa162c6a37c5164938035d43ea54c66ccb6336f
vanderwaal <grant.vanderwaal@gmail.com>
2026-02-15 18:36:17 +0700

    feat: enhance log package

--- log/logger.go
+++ log/logger.go
@@ -13,18 +13,37 @@ var (
 	g_logger *zap.Logger = nil
 
 	g_mapLevel = map[string]zapcore.Level{
-		"debug": zapcore.DebugLevel,
-		"info":  zapcore.InfoLevel,
-		"warn":  zapcore.WarnLevel,
-		"error": zapcore.ErrorLevel,
+		"debug":   zapcore.DebugLevel,
+		"info":    zapcore.InfoLevel,
+		"warn":    zapcore.WarnLevel,
+		"warning": zapcore.WarnLevel,
+		"error":   zapcore.ErrorLevel,
 	}
 )
 
-// maxSize: M
-// maxAge: day
-func newLogger(path, filename, level string, maxSize, maxAge, maxBackups int, localtime, compress bool) *zap.Logger {
-	w := zapcore.AddSync(&lumberjack.Logger{
+/*
+path: logfile path
+filename: log filename in @path. should not "error.log" which is for @errorFile
+level: one of debug/info/warn/warning/error
+maxSize: M
+maxAge: day
+maxBackups: number of most backups
+localtime: use local time
+compress: backups if compressed
+console: enable/disable console log.(DEBUG level)
+errorFile: save ERROR level logs to standalone `error.log`. meanwhile save to `main` log file too.
+*/
+func newLogger(path, filename, level string, maxSize, maxAge, maxBackups int, localtime, compress, console, errorFile bool) *zap.Logger {
+	writeSyncerAll := zapcore.AddSync(&lumberjack.Logger{
 		Filename:   fmt.Sprintf("%s/%s", path, filename),
+		MaxSize:    maxSize,
+		MaxAge:     maxAge,
+		MaxBackups: maxBackups,
+		LocalTime:  localtime,
+		Compress:   compress,
+	})
+	writeSyncerError := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   fmt.Sprintf("%s/error.log", path),
 		MaxSize:    maxSize,
 		MaxAge:     maxAge,
 		MaxBackups: maxBackups,
@@ -35,24 +54,36 @@ func newLogger(path, filename, level string, maxSize, maxAge, maxBackups int, lo
 	cfg := zap.NewProductionEncoderConfig()
 	cfg.EncodeTime = zapcore.ISO8601TimeEncoder
 	cfg.EncodeCaller = zapcore.FullCallerEncoder
-	cfg.CallerKey = "caller"
+	cfg.CallerKey = "fn"
 
 	lvl, ok := g_mapLevel[level]
 	if !ok {
 		lvl = zapcore.WarnLevel
 	}
-	core := zapcore.NewTee(
+	//
+	cores := []zapcore.Core{
 		zapcore.NewCore(
 			zapcore.NewJSONEncoder(cfg),
-			w,
+			writeSyncerAll,
 			lvl,
 		),
-		zapcore.NewCore(
+	}
+	if console {
+		cores = append(cores, zapcore.NewCore(
 			zapcore.NewConsoleEncoder(cfg),
 			zapcore.AddSync(os.Stdout),
-			lvl,
-		),
-	)
+			zapcore.DebugLevel,
+		))
+	}
+	if errorFile {
+		cores = append(cores, zapcore.NewCore(
+			zapcore.NewJSONEncoder(cfg),
+			writeSyncerError,
+			zapcore.ErrorLevel,
+		))
+	}
+
+	core := zapcore.NewTee(cores...)
 
 	g_logger = zap.New(core)
 
@@ -63,8 +94,8 @@ func newLogger(path, filename, level string, maxSize, maxAge, maxBackups int, lo
 
 // mod only interface.
 // @maxSize: M @maxAge days @maxBackup fileNums
-func GetLogger(path, filename, level string, maxSize, maxAge, maxBackups int, localtime, compress bool) *zap.Logger {
-	return newLogger(path, filename, level, maxSize, maxAge, maxBackups, localtime, compress)
+func GetLogger(path, filename, level string, maxSize, maxAge, maxBackups int, localtime, compress, console, errorFile bool) *zap.Logger {
+	return newLogger(path, filename, level, maxSize, maxAge, maxBackups, localtime, compress, console, errorFile)
 }
 
 func Close() error {
